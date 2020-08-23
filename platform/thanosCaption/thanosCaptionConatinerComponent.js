2e2893c86271484409ea05abb85738c6c3266187
askowronski <askowronski@backstopsolutions.com>
2020-08-23 11:13:38 -0500

    ET-36 Remove unused files

--- platform/thanosCaption/thanosCaptionConatinerComponent.js
+++
@@ -1,9 +0,0 @@
-import React  from 'react';
-import { ThanosCaptionView } from './thanosCaptionView';
-
-export const ThanosCaptionConatiner = () => {
-    const data = "Does this guy irritates you?"
-    return(
-        <ThanosCaptionView data={data}></ThanosCaptionView>
-    )
-}
